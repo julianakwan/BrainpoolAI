65c083c114b2c9f32eb380bb0b5348dabaa0ad98
julianakwan <j.kwan@ljmu.ac.uk>
2023-07-28 00:19:07 +0100

    calls OpenVino model

--- main.py
+++ main.py
@@ -2,6 +2,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 from openvino.runtime import Core
 from PIL import Image
+import os
+from app import app
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#        print('Hello World\n')
+        output_image = transform_image(filename)
+        output_filename = 'test.jpg'
+        output_image.save('static/uploads/'+output_filename)
+
+        flash('Image successfully uploaded and displayed below')
+        return render_template('upload.html', input_filename=filename, output_filename=output_filename)
+    else:
+       	flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
 def normalize_minmax(data):
@@ -15,8 +57,7 @@ def to_rgb(image_data) -> np.ndarray:
     """
     return cv2.cvtColor(image_data, cv2.COLOR_BGR2RGB)
 
-
-if __name__ == '__main__':
+def transform_image(image_name):
     model_path = "/Users/jkwan/Documents/jobs/BrainpoolAI/public/fast-neural-style-mosaic-onnx/fast-neural-style-mosaic-onnx.onnx"
     core = Core()
     core.set_property({'CACHE_DIR': '.../cache'})
@@ -30,10 +71,11 @@ if __name__ == '__main__':
     network_input_shape = list(input_key.shape)
     network_image_height, network_image_width = network_input_shape[2:]
 
-    image_file = "/Users/jkwan/Documents/jobs/BrainpoolAI/coco_bike.jpg"
+#    image_file = "/Users/jkwan/Documents/jobs/BrainpoolAI/coco_bike.jpg"
+    image_file = "/Users/jkwan/Documents/jobs/BrainpoolAI/static/uploads/"+image_name
     input_image = Image.open(image_file)
-    plt.imshow(input_image)
-    plt.show()
+#    plt.imshow(input_image)
+#    plt.show()
 
     #resize the image for the model dimensions
     input_image = input_image.resize((network_image_height, network_image_width))
@@ -52,10 +94,17 @@ if __name__ == '__main__':
 
     output_image = Image.fromarray(result.astype("uint8"), 'RGB')
     
+    return output_image    
 #could resize image again    
 #    output_image.resize(
-    plt.imshow(output_image)
-    plt.show()
+#    plt.imshow(output_image)
+#    plt.show()
+
+
+
+
+if __name__ == '__main__':
+    app.run()
 
     
     
